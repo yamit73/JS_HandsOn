8d28a38db3a287f55c30cd83e2525d5946911cb6
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 13:04:20 +0530

    Return a Value from a Function with Return  task-51

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
-function functionWithArgs(param1, param2) {
-    console.log(param1+param2);
+function timesFive(num) {
+    return num *5;
   }
-  functionWithArgs(1,2);
-  functionWithArgs(7,9);
+  timesFive(5) ;
+  timesFive(2) ;
+  timesFive(0) ;
