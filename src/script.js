07debf7be376965f059627e81a237cc79b727760
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 12:53:46 +0530

    Manipulate Arrays With unshift() task-47

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 // Setup
 const myArray = [["John", 23], ["dog", 3]];
-const removedFromMyArray=myArray.shift(["John", 23]);
+myArray.shift();
+myArray.unshift(["Paul", 35]);
 // Only change code below this line
