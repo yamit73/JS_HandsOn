db2dd10944b480956875eab21dd51ac24a6f16f3
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 12:45:07 +0530

    Access Multi-Dimensional Arrays With Indexes task-43

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
-// Setup
-const myArray = [18, 64, 99];
-myArray[0] = 45;
-myArray[1] = 64;
-myArray[2] = 99;;
-// Only change code below this line
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
