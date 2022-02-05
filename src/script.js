fe913f0984058f712aad95dabd82acb9dc0fdd71
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 14:19:35 +0530

    Chaining If Else Statements task-74

--- src/script.js
+++ src/script.js
@@ -1,12 +1,21 @@
-function orderMyLogic(val) {
-    if (val < 5) {
-      return "Less than 5";
-    } else if (val < 10) {
-      return "Less than 10";
-    } else {
-      return "Greater than or equal to 10";
-    }
+function testSize(num) {
+    // Only change code below this line
+    if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+      return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  
+  } else if (num < 20) {
+    return "Large";
+  }
+    else {
+    return "Huge";
   }
   
-  // Change this value to test
-  orderMyLogic(7);
+    return "Change Me";
+    // Only change code above this line
+  }
+  
+  testSize(7);
