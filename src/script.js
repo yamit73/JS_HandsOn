df55a05c7c129b5113633160aacb7bc2329c7c87
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 13:26:33 +0530

    Stand in Line task-57

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
-// Setup
-let processed = 0;
-
-function processArg(num) {
-  return (num + 3) / 5;
-}
-processed = processArg(7);
-// Only change code below this line
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    item=arr.shift(item);
+    return item;
+    // Only change code above this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
