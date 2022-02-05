959cc8f7ca15ddc35ba254efb149e506fa6b57cb
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 14:52:59 +0530

    Golf Code task-75

--- src/script.js
+++ src/script.js
@@ -1,21 +1,26 @@
-function testSize(num) {
-    // Only change code below this line
-    if (num < 5) {
-    return "Tiny";
-  } else if (num < 10) {
-      return "Small";
-  } else if (num < 15) {
-    return "Medium";
-  
-  } else if (num < 20) {
-    return "Large";
-  }
-    else {
-    return "Huge";
-  }
-  
-    return "Change Me";
-    // Only change code above this line
-  }
-  
-  testSize(7);
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+    if(strokes==1){
+      return names[0];
+    }
+    else if(strokes<=par-2){
+      return names[1];
+    }else if(strokes==par-1){
+      return names[2];
+    }else if(strokes==par){
+      return names[3]
+    }else if(strokes==par+1){
+      return names[4]
+    }else if(strokes==par+2){
+      return names[5]
+    }else {
+      return names[6]
+    }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
