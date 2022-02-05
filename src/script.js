24c9f17dbff32b00530febe5da909268b4bf84f3
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 12:43:18 +0530

    modify Array Data with Indexes task-42

--- src/script.js
+++ src/script.js
@@ -1,2 +1,6 @@
-const myArray = [50, 60, 70];
-let myData=myArray[0];
+// Setup
+const myArray = [18, 64, 99];
+myArray[0] = 45;
+myArray[1] = 64;
+myArray[2] = 99;;
+// Only change code below this line
