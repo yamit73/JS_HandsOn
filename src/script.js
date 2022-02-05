41000fd336d757a27fd11c7e7da8507766c1dceb
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 13:33:42 +0530

    Comparison with the Equality Operator task-60

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
-function trueOrFalse(wasThatTrue) {
-    // Only change code below this line
-    if (wasThatTrue==true) {
-      return "Yes, that was true";
-    }else{
-      return "No, that was false";
+// Setup
+function testEqual(val) {
+    if (val==12) { // Change this line
+      return "Equal";
     }
+    return "Not Equal";
   }
   
+  testEqual(10);
