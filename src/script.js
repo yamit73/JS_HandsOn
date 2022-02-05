b57d5637e713a22665a07d9ed8ace097ffecd87b
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 13:18:33 +0530

    Assignment with a Returned Value task-56

--- src/script.js
+++ src/script.js
@@ -1,16 +1,8 @@
 // Setup
-let sum = 0;
+let processed = 0;
 
-function addThree() {
-  sum = sum + 3;
+function processArg(num) {
+  return (num + 3) / 5;
 }
-
+processed = processArg(7);
 // Only change code below this line
-function addFive() {
-  sum = sum + 5;
-}
-
-// Only change code above this line
-
-addThree();
-addFive();
