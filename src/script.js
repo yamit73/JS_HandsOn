9aad862a9cb6e23ee659f5069267af712529bcf3
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 12:47:07 +0530

    Manipulate Arrays With push() task-44

--- src/script.js
+++ src/script.js
@@ -1,8 +1,4 @@
-const myArray = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [[10, 11, 12], 13, 14],
-  ];
-  
-  const myData = myArray[2][1];
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+// Only change code below this line
