39e66cb9051b6bc0eebc603514dfd196875d4a49
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 13:50:09 +0530

    Comparison with the Less Than Or Equal To Operator task-68

--- src/script.js
+++ src/script.js
@@ -1,13 +1,13 @@
-function testLessThan(val) {
-    if (val<25) {  // Change this line
-      return "Under 25";
+function testLessOrEqual(val) {
+    if (val<=12) {  // Change this line
+      return "Smaller Than or Equal to 12";
     }
   
-    if (val<55) {  // Change this line
-      return "Under 55";
+    if (val<=24) {  // Change this line
+      return "Smaller Than or Equal to 24";
     }
   
-    return "55 or Over";
+    return "More Than 24";
   }
   
-  testLessThan(10);
+  testLessOrEqual(10);
