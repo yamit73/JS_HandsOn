b33fce7cce1cf839e509b8c3fc014bcd5970278b
Amit Yadav <amityadav@cedcoss.com>
2022-02-05 12:31:35 +0530

    Word Blanks task-38

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
-// Setup
-const lastName = "Lovelace";
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
 
 // Only change code below this line
-const thirdLetterOfLastName = lastName[lastName.length-2]; // Change this line
+const wordBlanks = "i have a "+myNoun+" and it is "+myVerb+" and it "+myAdjective+" so "+myAdverb; // Change this line
+// Only change code above this line
