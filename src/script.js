4e1705716ef8cf5341164b071daf67dfed25d31b
Amit Yadav <amityadav@cedcoss.com>
2022-02-07 09:45:28 +0530

    Return Early Pattern for Functions

--- src/script.js
+++ src/script.js
@@ -1,7 +1,14 @@
-function isLess(a, b) {
-    // Only change code below this line
-    return a<b;
-    // Only change code above this line
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+
+  if(a<0 ||b<0){
+    return ;
   }
-  
-  isLess(10, 15);
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
