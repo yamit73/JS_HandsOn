fa2be64e9f34f6667cf9c8bd6c6361783276efee
Amit Yadav <amityadav@cedcoss.com>
2022-02-07 10:12:23 +0530

    Counting Cards task-82

--- src/script.js
+++ src/script.js
@@ -1,14 +1,18 @@
-// Setup
-function abTest(a, b) {
+let count = 0;
+
+function cc(card) {
   // Only change code below this line
 
-  if(a<0 ||b<0){
-    return ;
+  if(card==10 ||card=='A'||card=='Q'||card=='K'||card=='J'){
+    count--;
+  }else if(card==2 ||card==3||card==4||card==5||card==6){
+    count ++;
+  }else if(card==7 ||card==8||card==9){};
+  if(count>0){
+    return count+" Bet";
   }
-
+  return count+" Hold";
   // Only change code above this line
-
-  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-abTest(2,2);
+cc(2); cc(3); cc(7); cc('K'); cc('A');
